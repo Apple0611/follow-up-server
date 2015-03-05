734131d468aa207901ccb5be3a7755a2223725cb
Mingdong Luo <mdluo@nsmss.com>
2015-03-05 23:59:09 +0800

    Add Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
   resources :disease_categories
 
   # The priority is based upon order of creation: first created -> highest priority.
