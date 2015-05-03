120741a1c668fbd7936d1295bd9d703400b1438a
mdluo <mdluo@nsmss.com>
2015-05-03 10:24:11 +0800

    Add type column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502111729) do
+ActiveRecord::Schema.define(version: 20150503021447) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",           limit: 255
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20150502111729) do
     t.string   "activation_state",             limit: 255
     t.string   "activation_token",             limit: 255
     t.datetime "activation_token_expires_at"
+    t.string   "type",                         limit: 255
   end
 
   add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
