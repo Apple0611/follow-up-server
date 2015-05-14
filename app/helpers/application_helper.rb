c06d1f7b29367df6da8b2f2bb6518d9a70414d00
mdluo <mdluo@nsmss.com>
2015-05-14 21:47:15 +0800

    Customised will_paginate helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,14 @@ module ApplicationHelper
     title = @page_title ? "#{@page_title} | #{site_name}" : site_name rescue "SITE_NAME"
   end
 
+  def will_paginate(collection_or_options = nil, options = {})
+    if collection_or_options.is_a? Hash
+      options, collection_or_options = collection_or_options, nil
+    end
+    unless options[:renderer]
+      options = options.merge :renderer => PaginateLinkRenderer
+    end
+    super *[collection_or_options, options].compact
+  end
+
 end
