ce3b65e627cf4085b8ef68e49a3786a53684d311
mdluo <mdluo@nsmss.com>
2015-09-21 13:39:47 +0800

    Update User model validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,20 @@ class User < ActiveRecord::Base
     ROLES.index(base_role.to_s) <= ROLES.index(role)
   end
 
-  validates :email, presence: {
+  validates :email,
+  presence: {
     message: "邮箱或电话号码不能为空"
-  }
-
-  validates :email, uniqueness: {
+  },
+  uniqueness: {
     message: "此邮箱或电话号码已注册"
   }
+  
   validates :password, length: {
     in: 6..20,
     too_short: "密码至少需6位",
     too_long: "密码至多20位"
   }
+
   validates :term, acceptance: {
     message: "必须同意服务条款"
   }
