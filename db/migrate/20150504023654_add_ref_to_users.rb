454bc88f9af4b8ace30e8c3a23180c81ff89f094
mdluo <mdluo@nsmss.com>
2015-09-24 20:30:39 +0800

    Rename migrate

--- db/migrate/20150504023654_add_ref_to_users.rb
+++ db/migrate/20150504023654_add_ref_to_users.rb
@@ -1,4 +1,4 @@
-class AddRefToUser < ActiveRecord::Migration
+class AddRefToUsers < ActiveRecord::Migration
   def change
     add_reference :users, :treatment, index: true
     add_reference :users, :docotr, index: true
