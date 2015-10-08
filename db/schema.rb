ca6ddb95b1616a6ce2e031615f824dacf24074e5
Mingdong Luo <mdluo@nsmss.com>
2015-10-09 00:05:40 +0800

    Run migration of users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008075929) do
+ActiveRecord::Schema.define(version: 20151008094317) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -66,5 +66,26 @@ ActiveRecord::Schema.define(version: 20151008075929) do
   end
 
   add_index "treatments", ["disease_id"], name: "index_treatments_on_disease_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                   limit: 48,  default: ""
+    t.string   "mobile",                 limit: 16,  default: ""
+    t.string   "email",                  limit: 128, default: ""
+    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "password_salt",          limit: 128, default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
 
 end
