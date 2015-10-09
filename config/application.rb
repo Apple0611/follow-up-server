3eac1a2f5a97d11fe95bc98afeb7fe3e267ec4de
Mingdong Luo <mdluo@nsmss.com>
2015-10-09 13:57:43 +0800

    Update i18n config for loading locale files in nested dictionaries

--- config/application.rb
+++ config/application.rb
@@ -17,17 +17,16 @@ module FollowUp
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :'zh-CN'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.i18n.default_locale = :"zh-CN"
-
-    config.generators do |g|
-      g.stylesheets false
-    end
+    # config.generators do |g|
+    #   g.stylesheets false
+    # end
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
       html_tag
