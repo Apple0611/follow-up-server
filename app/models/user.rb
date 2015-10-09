e94fc8c9ab33c84440780a7841b725a21ceca609
Mingdong Luo <mdluo@nsmss.com>
2015-10-09 19:17:41 +0800

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,51 +6,49 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, authentication_keys: [:login]
+  # devise :validatable, password_length: 0..128
 
   # Virtual attribute for authenticating by either mobile or email
   attr_accessor :login
 
   validates :login, presence: true
-  validates :password, length: { in: 6..20 }
+  validates :password, presence: true, length: { in: 6..128 }
   validate :login_validate
   validates :term, acceptance: true
 
+  before_create do
+    if login =~ @@mobile_regex
+      self.mobile = login
+    elsif login =~ @@email_regex
+      self.email = login
+    end
+  end
+
   # Allow users to Sign In using their mobile or email address
   # http://git.io/vCUn6
+  # warden_conditions == {login: "email or mobile"}
   def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(mobile) = :value OR lower(email) = :value", { value: login.downcase }]).first
-    else
-      where(conditions.to_hash).first
+    if login = warden_conditions[:login]
+      where(["lower(mobile) = :value OR lower(email) = :value", { value: login.downcase }]).first
     end
   end
 
   private
-
-  # Use 'login'(eighter email or mobile) as authentication_keys
-  # So email are not required for validation
-  # See more: http://git.io/vCTgV
-  def email_required?
-    return false
-  end
+  # China mobile phone number regex: http://git.io/vCtiM
+  @@mobile_regex = /(^(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7})$/
+  @@email_regex = Devise::email_regexp
 
   # Validate 'login' field for different format (email or mobile)
   # Docs of customed validation: http://guides.rubyonrails.org/active_record_validations.html#custom-methods
   # http://guides.rubyonrails.org/i18n.html#translations-for-active-record-models
   def login_validate
-    if login =~ /(^(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7})$/
-      if User.where(mobile: login.downcase).any?
-        errors.add(:login, :mobile_exists)
-      end
-    elsif login =~ /([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})/i
-      if User.where(email: login.downcase).any?
-        errors.add(:login, :email_exists)
-      end
+    if login =~ @@mobile_regex
+      errors.add :login, :mobile_exists if User.where(mobile: login.downcase).any?
+    elsif login =~ @@email_regex
+      errors.add :login, :email_exists if User.where(email: login.downcase).any?
     else
-      errors.add(:login, :invalid)
+      errors.add :login, :invalid
     end
   end
-
 end
