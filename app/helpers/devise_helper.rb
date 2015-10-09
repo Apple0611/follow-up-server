9a175b4f91438bd52220ec6feb63e8383089b1c6
Mingdong Luo <mdluo@nsmss.com>
2015-10-09 12:50:56 +0800

    Update User validation error helper and translation

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,9 +1,15 @@
+## Helper to create semantic-ui warning message
+## Override devise_error_messages! for views. http://git.io/vCL93
+## Author: Mingdong Luo (https://github.com/mdluo)
+## Date: Oct 09, 2015
+
 module DeviseHelper
   def devise_error_messages!
     return "" if resource.errors.empty?
 
     sentence = I18n.t 'errors.messages.error'
     message = resource.errors.messages.first[1][0]
+    # message = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
 
     html = <<-HTML
     <div class="ui warning message">
